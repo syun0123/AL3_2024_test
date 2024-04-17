a56bc3c2f025ae66d345b673e36ed59647eba1b5
syun0123 <146914916+syun0123@users.noreply.github.com>
2024-04-17 15:23:10 +0900

    test

--- DirectXGame/scene/GameScene.cpp
+++ DirectXGame/scene/GameScene.cpp
@@ -1,20 +1,42 @@
 #include "GameScene.h"
 #include "TextureManager.h"
 #include <cassert>
+#include"ImGuiManager.h"
 
 GameScene::GameScene() {}
 
-GameScene::~GameScene() {}
+GameScene::~GameScene() { 
+	// delete sprite_; 
+	delete model_;
+}
 
 void GameScene::Initialize() {
 
 	dxCommon_ = DirectXCommon::GetInstance();
 	input_ = Input::GetInstance();
 	audio_ = Audio::GetInstance();
+	textureHandle_ = TextureManager::Load("kamereon.jpeg");
+	//スプライトの生成
+	//sprite_ = Sprite::Create(textureHandle_, {100, 50});
+	//3D モデルの生成
+	model_ = Model::Create();
+	worldTransform_.Initialize();
+	viewProjection_.Initialize();
 }
 
-void GameScene::Update() {}
+void GameScene::Update() { /*
+	移動
+	Vector2 position = sprite_->GetPosition();
+	position.x += 2.0f;
+	position.y += 1.0f;
+	sprite_->SetPosition(position);*/
+	ImGui::Begin("Debug1");
+	//ImGui::Text("Kamata tarou %d,%d,%d,2050,12,31");
+	ImGui::InputFloat3("InputFloat3", inputFloat3);
+	ImGui::SliderFloat3("SliderFloat3", inputFloat3, 0.0f, 1.0f);
+	ImGui::End();
 
+}
 void GameScene::Draw() {
 
 	// コマンドリストの取得
@@ -41,7 +63,7 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
-
+	model_->Draw(worldTransform_, viewProjection_, textureHandle_);
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
 #pragma endregion
@@ -49,13 +71,14 @@ void GameScene::Draw() {
 #pragma region 前景スプライト描画
 	// 前景スプライト描画前処理
 	Sprite::PreDraw(commandList);
-
+	
 	/// <summary>
 	/// ここに前景スプライトの描画処理を追加できる
 	/// </summary>
-
+	/// 
+	//sprite_-> Draw();
 	// スプライト描画後処理
 	Sprite::PostDraw();
-
+	
 #pragma endregion
 }
--- DirectXGame/scene/GameScene.h
+++ DirectXGame/scene/GameScene.h
@@ -18,6 +18,12 @@ public: // メンバ関数
 	/// コンストクラタ
 	/// </summary>
 	GameScene();
+	//	ワールドトランスフォーム
+	WorldTransform worldTransform_;
+	// ビュープロダクション
+	ViewProjection viewProjection_;
+	//ImGuiで値を入力する変数
+	float inputFloat3[3] = {0, 0, 0};
 
 	/// <summary>
 	/// デストラクタ
@@ -26,6 +32,9 @@ public: // メンバ関数
 
 	/// <summary>
 	/// 初期化
+    /// 
+ 
+
 	/// </summary>
 	void Initialize();
 
@@ -47,4 +56,11 @@ private: // メンバ変数
 	/// <summary>
 	/// ゲームシーン用
 	/// </summary>
+	//テクスチャハンドル
+	uint32_t textureHandle_ = 0;
+	
+	//スプライト
+	//Sprite* sprite_ = nullptr;
+	Model* model_ = nullptr;
+
 };
